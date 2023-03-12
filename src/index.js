5e1e838c3e60cbb86b298a94357e5adcca459b7a
jhon3242 <rldotnr2@gmail.com>
2023-03-12 12:13:13 +0900

    feat : 메뉴 수정 기능 추가

--- src/index.js
+++ src/index.js
@@ -1,6 +1,23 @@
 // 변수로 정의해둬서 코드의 반복을 줄인다.
 const $ = (selector) => document.querySelector(selector);
 function App() {
+    /**
+     * Step2. 메뉴 수정
+     */
+    // 이벤트 위임 : 이벤트를 추가하는 시점에 해당 태그가 없기 떄문에 상위 태그에 이벤트를 위임
+    $("#espresso-menu-list").addEventListener("click", (e) => {
+        if (e.target.classList.contains("menu-edit-button")) {
+            // 현재 위치에서 상위로 올라가면서 가장 가까운 li 태그를 찾고 다시 그 아래 menu-name 쿼리를 선택
+            let $menuName = e.target.closest("li").querySelector(".menu-name");
+            let updatedMenuName = prompt("메뉴 이름을 수정해주세요.", $menuName.innerText);
+            $menuName.innerText = updatedMenuName;
+        }
+    })
+
+
+    /**
+     * Step1. 메뉴 추가
+     */
     // form 테그가 자동으로 전송되는 것을 막는다.
     $("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
@@ -15,20 +32,20 @@ function App() {
         let espressoMenuName = $("#espresso-menu-name").value;
         const menuItemTemplate = (espressoMenuName) => {
             return (`
-                    <li className="menu-list-item d-flex items-center py-2">
-                        <span className="w-100 pl-2 menu-name">${espressoMenuName}</span>
-                        <button
-                            type="button"
-                            className="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                        >
-                            수정
-                        </button>
-                        <button현
-                            type="button"
-                            className="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                        >
-                            삭제
-                        </button현>
+                    <li class="menu-list-item d-flex items-center py-2">
+                      <span class="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                      <button
+                        type="button"
+                        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                      >
+                        수정
+                      </button>
+                      <button
+                        type="button"
+                        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                      >
+                        삭제
+                      </button>
                     </li>
                 `);
         }
@@ -54,7 +71,8 @@ function App() {
                 return ;
             }
             addEspressoMenuName();
-        })
+        });
+
 }
 
 App();
