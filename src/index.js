dbecd7a23e1e0d3cd0970a14d2cc3675985ca0f2
jhon3242 <rldotnr2@gmail.com>
2023-03-12 11:45:28 +0900

    feat : 확인 버튼 추가 구현

--- src/index.js
+++ src/index.js
@@ -7,48 +7,53 @@ function App() {
     })
 
     // 메뉴의 이름을 입력 받는다.
+    function addEspressoMenuName() {
+        if ($("#espresso-menu-name").value === "") {
+            alert("값을 입력해주세요.")
+            return;
+        }
+        let espressoMenuName = $("#espresso-menu-name").value;
+        const menuItemTemplate = (espressoMenuName) => {
+            return (`
+                    <li className="menu-list-item d-flex items-center py-2">
+                        <span className="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                        <button
+                            type="button"
+                            className="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                        >
+                            수정
+                        </button>
+                        <button현
+                            type="button"
+                            className="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                        >
+                            삭제
+                        </button현>
+                    </li>
+                `);
+        }
+        // 기존에 있던 li 에 추가로 붙이기 위해선 insertAdjacentHTML 사용
+        $("#espresso-menu-list").insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName));
+
+        // 총 개수
+        let menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+        $(".menu-count").innerText = `총 ${menuCount} 개`;
+
+        // input 값 빈 값으로 초기화
+        $("#espresso-menu-name").value = "";
+    }
+
+    $("#espresso-menu-submit-button").addEventListener("click", () =>{
+        addEspressoMenuName();
+    })
+
+    // 엔터 키
     $("#espresso-menu-name")
         .addEventListener('keypress', (e) => {
             if (e.key !== 'Enter') {
                 return ;
             }
-            if ($("#espresso-menu-name").value === ""){
-                alert("값을 입력해주세요.")
-                return ;
-            }
-            if (e.key === 'Enter') {
-                let espressoMenuName = $("#espresso-menu-name").value;
-                const menuItemTemplate = (espressoMenuName) => {
-                    return (`
-                        <li className="menu-list-item d-flex items-center py-2">
-                            <span className="w-100 pl-2 menu-name">${espressoMenuName}</span>
-                            <button
-                                type="button"
-                                className="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
-                            >
-                                수정
-                            </button>
-                            <button현
-                                type="button"
-                                className="bg-gray-50 text-gray-500 text-sm menu-remove-button"
-                            >
-                                삭제
-                            </button현>
-                        </li>
-                    `);
-                }
-                // 기존에 있던 li 에 추가로 붙이기 위해선 insertAdjacentHTML 사용
-                $("#espresso-menu-list").insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName));
-
-                // 총 개수
-                let menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
-                $(".menu-count").innerText = `총 ${menuCount} 개`;
-
-                // input 값 빈 값으로 초기화
-                $("#espresso-menu-name").value = "";
-
-            }
-
+            addEspressoMenuName();
         })
 }
 
