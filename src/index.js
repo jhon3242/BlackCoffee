3450c38653372a6dfcaefcd5fad9becad1c50f9f
jhon3242 <rldotnr2@gmail.com>
2023-03-12 11:24:19 +0900

    feat : 총 개수 표현 기능 구현

--- src/index.js
+++ src/index.js
@@ -1,17 +1,43 @@
-
+// 변수로 정의해둬서 코드의 반복을 줄인다.
+const $ = (selector) => document.querySelector(selector);
 function App() {
     // form 테그가 자동으로 전송되는 것을 막는다.
-    document.querySelector("#espresso-menu-form").addEventListener("submit", (e) => {
+    $("#espresso-menu-form").addEventListener("submit", (e) => {
         e.preventDefault();
     })
 
     // 메뉴의 이름을 입력 받는다.
-    document
-        .querySelector("#espresso-menu-name")
+    $("#espresso-menu-name")
         .addEventListener('keypress', (e) => {
             if (e.key === 'Enter') {
-                console.log(document.querySelector("#espresso-menu-name").value)
+                let espressoMenuName = $("#espresso-menu-name").value;
+                const menuItemTemplate = (espressoMenuName) => {
+                    return (`
+                        <li className="menu-list-item d-flex items-center py-2">
+                            <span className="w-100 pl-2 menu-name">${espressoMenuName}</span>
+                            <button
+                                type="button"
+                                className="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+                            >
+                                수정
+                            </button>
+                            <button
+                                type="button"
+                                className="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+                            >
+                                삭제
+                            </button>
+                        </li>
+                    `);
+                }
+                // 기존에 있던 li 에 추가로 붙이기 위해선 insertAdjacentHTML 사용
+                $("#espresso-menu-list").insertAdjacentHTML('beforeend', menuItemTemplate(espressoMenuName));
+
+                let menuCount = $("#espresso-menu-list").querySelectorAll("li").length;
+                $(".menu-count").innerText = `총 ${menuCount} 개`;
+
             }
+
         })
 }
 
