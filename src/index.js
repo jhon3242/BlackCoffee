4eb932f10d666afb282112a5e77eec01a43df8bb
jhon3242 <rldotnr2@gmail.com>
2023-03-15 15:11:36 +0900

    feat : 메뉴 추가시 리스트 추가 구현

--- src/index.js
+++ src/index.js
@@ -7,11 +7,32 @@ function App() {
         espresso : []
     };
     this.category = "espresso";
+    const template = (name) => `
+        <li class="menu-list-item d-flex items-center py-2">
+      <span class="w-100 pl-2 menu-name">${name}</span>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm mr-1 menu-edit-button"
+      >
+        수정
+      </button>
+      <button
+        type="button"
+        class="bg-gray-50 text-gray-500 text-sm menu-remove-button"
+      >
+        삭제
+      </button>
+    </li>`
+    const render = () => {
+        const menuList = this.state[this.category].map(template).join("");
+        $("#espresso-menu-list").innerHTML = menuList;
+    }
 
     const addMenu = (menuName) => {
         this.state[this.category].push(menuName);
-        console.log(this.state[this.category])
+        render();
     }
+
 
 
     // form 테그가 자동으로 전송되는 것을 막는다.
